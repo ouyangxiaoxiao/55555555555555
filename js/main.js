fe6b2cc8f8b0b44295d59b244f5a5c952ef01bed
ouyangxiaoxiao <64386801+ouyangxiaoxiao@users.noreply.github.com>
2020-05-09 12:02:07 +0800

    22

--- js/main.js
+++ js/main.js
@@ -37,8 +37,59 @@ window.onload = function(){
         }
 
     }
+//收起播放列表
+    ctrl.onclick = function(){
 
+        aside.style.transition = "1s";
+        aside.style.transform = "translateX(-10vw)";
+        setTimeout(function(){
+            aside.style.display = "none";
+            ctrl_show.style.visibility= 'visible';
+        },"1000");
 
     }
+
+    //展开播放列表
+    ctrl_show.onclick = function(){
+        aside.style.display = "block";
+        ctrl_show.style.visibility= 'hidden';
+        setTimeout(function(){
+            aside.style.transform = "translateX(0vw)";
+        },"0");
+
+    }
+
+    video.setAttribute('src',url[0]);
+    lis[0].innerHTML = lis[j].innerHTML+'正在播放 ';
+    lis[0].className = "select";
+
+
+
+    video.addEventListener('ended', play);
+    //play();
+    function play() {
+        video.src = url[curr];
+        video.load(); // 如果短的话，可以加载完成之后再播放，监听 canplaythrough 事件即可
+        video.play();
+
+        for(var j=0;j<lis.length;j++){
+            if(j == curr){
+                video.setAttribute("src",lis[j].getAttribute("value"));
+                video.setAttribute('autoplay','autoplay');
+                lis[j].innerHTML = lis[j].innerHTML+'正在播放 ';
+                lis[j].className = "select";
+            }else{
+                lis[j].innerHTML = lis[j].getAttribute("title");
+                lis[j].className = "";
+            }
+        }
+        curr++;
+        if (curr >= vLen) curr = 0; // 播放完了，重新播放
+    }
+
+}
+
+
+
 
 
